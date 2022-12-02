f1343086bfc679467f025b608919c2d3a6c8fcf9
Iryna Yehorova <yehorova.i@advabet.com>
2022-12-02 20:56:09 +0200

    add array functions

--- before/js/index.js
+++ before/js/index.js
@@ -1,23 +1,38 @@
 // variables
 
 var smth;
+// между ними разница в областях видимости
+// var будет видно везде (hoisting) - переменная будет создано сначала, но значение присвоено в моменте -> null
 let smth1 = 2;
-const smth2 = smth1 * 3;
+const smth2 = smth1 * 3;// num не изменится
+const smth3 = [1]
+smth3.push(2) // [1, 2]
+smth3.length = 0 // []
+// smth3 = 2 // do not work!!
+// у них другая область видимости {}, не поднимаются -> undefined
+// let изменяется
+// сonst - константа для простых типов, а для массива и объекта тожет изменятся, но не переназначиваться
 // console.log(smth2)
 
 // types of variables
 let str = "string";
-let num = "2";
-let arr = ["65465t", "hgfjhf", "f06", 12, {}];
+let num = 2;
 let bool = false;
+let simple1 = null;
+let simple2 = undefined;
+
+let arr = ["65465t", "hgfjhf", "f06", 12, {}, []];
 let obj = {
   "name-id": "Irina",
   arr1: arr,
   obj: {
     obj: {},
+    arr: []
   },
 };
-// undefined, null
+
+// Symbol
+// BigInt
 
 // console.log(str[3])
 // console.log(arr[2])
@@ -26,27 +41,27 @@ let obj = {
 
 // =,  ==, ===
 
-// console.log(1 == '1')
-// console.log(1 === '1')
+// console.log(1 == '1') - true
+// console.log(1 === '1') - false
 
 // >, >=, >== !==
 
 // if (typeof str === 'boolean') {
-//     console.log('string')
+//   console.log('string')
 // } else {
-//     console.log('not string')
+//   console.log('not string')
 // }
 
 // if (typeof str === 'boolean') {
-//     console.log('string')
+//   console.log('string')
 // } else if (typeof str ==='number ') {
-//     console.log('number')
+//   console.log('number')
 // } else {
-//     console.log('something else')
+//   console.log('something else')
 // }
 
 // if (typeof str === 'boolean') {
-//     console.log('string')
+//   console.log('string')
 // }
 
 // function addThree(num) {
@@ -75,13 +90,13 @@ let obj = {
 // }
 // console.log(hello('Luke'))
 
-function dartWaider(name) {
-  if (name === "Luke") {
-    console.log("I'm your father!");
-  } else {
-    console.log("Kill him!");
-  }
-}
+// function dartWaider(name) {
+//   if (name === "Luke") {
+//     console.log("I'm your father!");
+//   } else {
+//     console.log("Kill him!");
+//   }
+// }
 
 // dartWaider('Luke')
 
@@ -91,36 +106,119 @@ function dartWaider(name) {
 
 // // console.log(closeCredit(30000, 60000))
 
-// function buyNewCar (allPrice, monthPay) {
-//     return `You will buy new car after ${allPrice / monthPay} months`
-// }
+function buyNewCar (allPrice, monthPay) {
+    return `You will buy new car after ${allPrice / monthPay} months`
+}
 
 // console.log(buyNewCar(14, 7))
 
-/*let myWay = {
+
+// array functions
+const func1 = function (num) {
+  return num
+}
+func1(1)
+
+const func2 = function close (num) {
+  return num
+}
+func2(1)
+
+// const func3 = buyNewCar(100, 3)
+// func3()
+
+const func4 = () => {}
+const func5 = num => num + 20
+const func6 = (num1, num2) => {
+  let one = num1 + num2;
+  one ++;
+  return one;
+}
+
+const multiplyByThree = (num) => num * num * num
+const answer = multiplyByThree(5)
+
+
+const myWay = {
     distance: 800, 
     rate: 8,
     price: 52,
     fuel: 'electro',
     'car-id': 563
 } 
-for (let key in myWay) {
-    console.log(key + ": " + myWay[key])
+
+// принимает расстояние которое нужно проехать, а показывает сколько это стоит
+
+const totalPrice = distance => distance / myWay.rate * myWay.price
+// console.log(totalPrice(60))
+
+const totalPrice2 = (distance, price) => distance / myWay.rate * price
+
+const totalPrice3 = (distance, price) => {
+  console.log(`You need ${distance / myWay.rate * price} $ to finish the trip`)
 }
 
-const wayDistance = myWay.distance
-const id = myWay['car-id']
+const persons = [{
+  name: 'Amy',
+  job: 'engineer',
+  age: 25,
+  female: true
+},
+{
+  name: 'Rory',
+  job: 'doctor',
+  age: 26,
+  female: false
+},
+{
+  name: 'Doctor',
+  job: 'doctor',
+  age: 1125,
+  female: false
+}]
+// принимает массив с людьми, считаешь сколько там мужчин, 
+//средний возраст и есть ли люди с одинаковой профессие и все это 
+//возвращаешь в консоль 
+// всего 3 человека, из них 2 мужчин Средний возраст - н . Есть/нет с одинаковой профессией
+const mansInArr = (persons) => {
+  let numberOfMen = 0;
+  let totalAge = 0;
+  let allJob = [];
 
-function myWayCost(item) {
-    if (item.fuel === 'electro' && item.distance > 300){
-        console.log('You shall not pass dat distance, you need portable generator')
-    } else {
-        console.log( `You need ${item.distance / item.rate * item.price}UAH to ride ${item.distance} kilometers`)
+  for (let person in persons) {
+    if (persons[person].female === false) {
+      numberOfMen += 1
     }
+    totalAge += persons[person].age
+    allJob.push(persons[person].job)
+  }
 
+  return `The total number of people is ${persons.length},
+    among them there are ${numberOfMen} men. 
+    The average age is ${totalAge / persons.length} years old. 
+    And it's ${allJob.length !== [...new Set(allJob)].length} 
+    dat we have peoples with same job`
 }
 
-myWayCost(myWay) */
+console.log(mansInArr(persons))
+
+// for (let key in myWay) {
+//     console.log(key + ": " + myWay[key])
+// }
+
+// const wayDistance = myWay.distance
+// const id = myWay['car-id']
+
+// function myWayCost(item) {
+//     if (item.fuel === 'electro' && item.distance > 300){
+//         console.log('You shall not pass dat distance, you need portable generator')
+//     } else {
+//         console.log( `You need ${item.distance / item.rate * item.price}UAH to ride ${item.distance} kilometers`)
+//     }
+
+// }
+
+// myWayCost(myWay) 
 
 // function displayNumbers(num){
 //     for (let i = 1;  i <= num; i++){
@@ -223,4 +321,4 @@ let secondSide = 4;
 function findThirdSide() {
   return Math.sqrt(firstSide * firstSide + secondSide * secondSide);
 }
-console.log(findThirdSide());
+// console.log(findThirdSide());
